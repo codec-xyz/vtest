c8b7d469da5d14aac834fdc0789caa347e8d2979
codec-xyz <112908328+codec-xyz@users.noreply.github.com>
2024-11-15 17:29:07 -0500

    Fixes MacOS title bar overlay error (#001)

--- src/main.ts
+++ src/main.ts
@@ -120,6 +120,9 @@ ipcMain.on('toggleDevTools', (event) => event.sender.toggleDevTools());
 ipcMain.on('setTitleBarColors', (event, bgColor, iconColor) => {
 	const window = BrowserWindow.fromWebContents(event.sender);
 	if(window === null) return;
+	
+	// MacOS title bar overlay buttons do need styling so the function is undefined
+	if(window.setTitleBarOverlay === undefined) return;
 
 	window.setTitleBarOverlay({
 		color: bgColor,
