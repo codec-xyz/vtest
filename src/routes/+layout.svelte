34480b952e2261a771c377833333e1c957733894
FAP-Foundation <info@fap.foundation>
2024-11-16 14:56:57 -0700

    Setting up shadcn-svelte and cross platform stuff. BROKEN, lots to fix.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,34 @@
 <script lang="ts">
-	import '../app.css';
-	import Devtools from '$lib/Devtools.svelte';
-	import { preferredTheme } from '$lib/preferredTheme.svelte';
+	import "../app.css";
+	import Devtools from "$lib/components/Devtools.svelte";
+	import { preferredTheme } from "$lib/preferredTheme.svelte";
 
 	let { children } = $props();
 
 	$effect(() => {
-		if(preferredTheme.theme === 'dark') window.setTitleBarColors('#374151', '#f8fafc');
-		else window.setTitleBarColors('#e5e7eb', '#020617');
+		if (preferredTheme.theme === "dark")
+			window.setTitleBarColors("#374151", "#f8fafc");
+		else window.setTitleBarColors("#e5e7eb", "#020617");
 	});
 </script>
 
-<div id='titlebar' class='shrink-0 bg-gradient-to-r from-gray-100 to-gray-200 flex text-slate-950 dark:from-[#273141] dark:to-gray-700 dark:text-slate-50'>
-	<div class='px-4 select-none grow text-xs flex items-center' style='-webkit-app-region: drag;'>Example App</div>
+<div
+	id="titlebar"
+	class="shrink-0 bg-gradient-to-r from-gray-100 to-gray-200 flex text-slate-950 dark:from-[#273141] dark:to-gray-700 dark:text-slate-50"
+>
+	<div
+		class="px-4 select-none grow text-xs flex items-center"
+		style="-webkit-app-region: drag;"
+	>
+		Example App
+	</div>
 	{#if import.meta.env.DEV}
-		<Devtools/>
+		<Devtools />
 	{/if}
 </div>
-<div class='overflow-auto h-full bg-gradient-to-br from-white to-zinc-50 text-slate-950 dark:from-zinc-800 dark:to-zinc-900 dark:text-slate-50'>
+<div
+	class="overflow-auto h-full bg-gradient-to-br from-white to-zinc-50 text-slate-950 dark:from-zinc-800 dark:to-zinc-900 dark:text-slate-50"
+>
 	{@render children()}
 </div>
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
@@ -13,14 +15,12 @@ const config = {
 				'script-src': ['self']
 			},
 		},
-
 		paths: {
 			relative: false,
 		},
-
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		alias: {
+			$lib: path.resolve(__dirname, './src/lib')
+		},
 		adapter: adapter({
 			// SPA (Single-page application)
 			// https://kit.svelte.dev/docs/single-page-apps
