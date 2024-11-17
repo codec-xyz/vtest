3158886c08674d227bde6c788dff21f8546c36ee
codec-xyz <112908328+codec-xyz@users.noreply.github.com>
2024-11-17 16:35:02 -0500

    Fixes comment typo for: MacOS title bar overlay error (#001)

--- src/main.ts
+++ src/main.ts
@@ -121,7 +121,7 @@ ipcMain.on('setTitleBarColors', (event, bgColor, iconColor) => {
 	const window = BrowserWindow.fromWebContents(event.sender);
 	if(window === null) return;
 	
-	// MacOS title bar overlay buttons do need styling so the function is undefined
+	// MacOS title bar overlay buttons do not need styling so the function is undefined
 	if(window.setTitleBarOverlay === undefined) return;
 
 	window.setTitleBarOverlay({
